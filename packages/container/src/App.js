ad6b0afe1963dbf442f8db91b27dd33f2355d0ac
Victor <victor_993@msn.com>
2021-04-12 20:12:54 +0300

    updated import

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,18 +1,18 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
-} from '@material-ui/core/styles';
+} from "@material-ui/core/styles";
 
-import Progress from './components/Progress';
-import Header from './components/Header';
+import Progress from "./components/Progress";
+import Header from "./components/Header";
 
-const MarketingLazy = lazy(() => import('./components/MarketingApp'));
-const AuthLazy = lazy(() => import('./components/AuthApp'));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+import MarketingApp from "./components/marketingApp";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co',
+  productionPrefix: "co",
 });
 
 export default () => {
@@ -28,10 +28,10 @@ export default () => {
           />
           <Suspense fallback={<Progress />}>
             <Switch>
-              <Route path="/auth">
+              <Route path='/auth'>
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
-              <Route path="/" component={MarketingLazy} />
+              <Route path='/' component={MarketingLazy} />
             </Switch>
           </Suspense>
         </div>
