d3fc602db244f59db47ba4a03a85cee0d4ee02f9
Victor <victor_993@msn.com>
2021-04-13 10:11:11 +0300

    updated workflow

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
-console.log('hello')
+console.log('hello!')
 
 // Mount function to start up the app
 const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
